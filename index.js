e5b8055ec0ead6ac4a1a292e74cb5b951dff8dfc
mkla68wi <78868374+mkla68wi@users.noreply.github.com>
2021-02-15 17:11:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var http = require('http');
 var url = require("url");
 
-var server = require("./ server");
-var router = require("./ router");
+var server = require("./server");
+var router = require("./router");
 
 const hostname = '127.0.0.1';
 const port = 3000;
