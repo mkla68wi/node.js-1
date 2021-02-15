b0f552934cdd9c71f13bab8ca0f5c99073ea7c6a
mkla68wi <78868374+mkla68wi@users.noreply.github.com>
2021-02-15 17:04:10 +0100

    index.js
    next try

---
+++ index.js
@@ -0,0 +1,34 @@
+/* MK first try
+*/
+var http = require('http');
+var url = require("url");
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+function start(route){
+  function route (pathname){
+    console.log("About to route a request for: " + pathname);
+  };
+  
+  console.log ("Start Function");
+  
+  function onRequest(request, response){
+    var pathname = url.parse (request.url) .pathname;      
+    //console.log ("Request for " + pathname + " received.");
+    
+    route (pathname);
+    
+    response.writeHead (200, {"Content-Type": "text / html"});
+    
+    response.write ("Hello World");      
+    
+    response.end ();
+  };
+  http.createServer (onRequest) .listen (3000);    
+  console.log ("Server Started.");
+};
+
+console.log ("Start Server");
+
+exports.start = start ();
