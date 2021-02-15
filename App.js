af675c587b4f5049bb9c88f7ecca8e387aa7934f
mkla68wi <78868374+mkla68wi@users.noreply.github.com>
2021-02-15 08:23:31 +0100

    Update App. New function
    2. Test

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 /* MK first try
 */
-const http = require('http');
+var http = require('http');
+//const url = require("url");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+function start(){
+  function onRequest(request, response){
+    var pathname = url.parse (request.url) .pathname;      
+    console.log ("Request for" + pathname + "received.");
+    
+    response.writeHead (200, {"Content-Type": "text / html"});
+    
+    response.write ("Hello World");      
+    
+    response.end ();
+  };
+  http.createServer (onRequest) .listen (port);    
+  console.log ("Server Started.");
+};
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+exports.start = start;
